964abdcb2af1997e33a33df6597ee2f283f918f3
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-05-18 10:56:09 +0300

    Fixed merge issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import DemonList from './pages/DemonList'
+import Auth from './pages/Auth';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/list" element={<DemonList />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 }
