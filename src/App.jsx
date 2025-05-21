2a599aee9319593f8fddc01bf8027589fd1ecf13
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-05-21 10:32:38 +0300

    Level view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DemonList from './pages/DemonList'
 import Auth from './pages/Auth';
+import Level from './pages/Level'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/list" element={<DemonList />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/level/:position" element={<Level/>}/>
     </Routes>
   );
 }
