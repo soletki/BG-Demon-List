f31a97751e79fd8051022dc48bdaace91bdef306
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-05-21 12:30:56 +0300

    List page is now default page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
 import DemonList from './pages/DemonList'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/list" element={<DemonList />} />
+      <Route path="/" element={<DemonList />} />
     </Routes>
   );
 }
