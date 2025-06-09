bed2dbf00e6c840572de84d4d96019df4b1ef3e8
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-06-09 18:14:48 +0300

    leaderboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import DemonList from './pages/DemonList'
 import Auth from './pages/Auth';
 import Level from './pages/Level'
+import Leaderboard from './pages/Leaderboard';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<DemonList />} />
       <Route path="/list" element={<DemonList />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
       <Route path="/level/:position" element={<Level/>}/>
     </Routes>
   );
