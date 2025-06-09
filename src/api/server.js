3d65725d68f3243f49cf1845308a4469f856188c
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-06-09 18:33:37 +0300

    Added an endpoint for adding a user

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import levelsRouter from './routes/levels.js';
 import playersRouter from './routes/players.js'
+import usersRouter from './routes/users.js'
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 // Routes
 app.use('/levels', levelsRouter);
 app.use('/players', playersRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
 	console.log(`Server has started on http://localhost:${port}`);
