f8d89446770b136bb9964e4be44a5bcdde17f99b
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-06-14 17:39:56 +0300

    Account page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Level from './pages/Level';
 import Leaderboard from './pages/Leaderboard';
 import Navbar from './components/Navbar';
 import Admin from './pages/Admin';
+import Account from './pages/Account';
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 				<Route path="/auth" element={<Auth />} />
 				<Route path="/leaderboard" element={<Leaderboard />} />
 				<Route path="/submit" element={<RecordSubmission />} />
+				<Route path="/account" element={<Account />} />
 				<Route path="/level/:position" element={<Level />} />
 			</Routes>
 		</div>
