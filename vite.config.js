85b80944e77e22ae66254d125a9f5cb44f90d62d
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-06-16 16:17:06 +0300

    Moved backend url to .env file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/levels': 'http://localhost:3000',
-      '/records': 'http://localhost:3000',
-      '/users': 'http://localhost:3000',
-      '/players': 'http://localhost:3000',
-      '/claims': 'http://localhost:3000',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const backendUrl = env.VITE_BACKEND_URL;
+  const proxyPaths = ['/levels', '/records', '/users', '/players', '/claims'];
+
+  const proxy = Object.fromEntries(
+    proxyPaths.map((path) => [path, { target: backendUrl, changeOrigin: true }])
+  );
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy,
     },
-  },
+  };
 });
