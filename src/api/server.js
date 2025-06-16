41b63f710c4d99781103789d8ae525dfca85efa2
Stoyan Ivanov <101361817+soletki@users.noreply.github.com>
2025-06-16 17:02:54 +0300

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: [process.env.VITE_BACKEND_URL, 'http://localhost:5173'],
+  origin: [process.env.VITE_FRONTEND_URL, 'http://localhost:5173'],
   credentials: true
 }));
 
